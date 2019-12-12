8d2125d2ec9e0177fd7ca6c83eb5a031e1815cf2
Jasper van Vliet <jvliet6@avans.nl>
2019-12-12 10:56:05 +0100

    Start of my-boards

--- src/app/components/boards/my-boards/my-boards.component.ts
+++ src/app/components/boards/my-boards/my-boards.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BoardService} from '../../../services/board.service';
 
 @Component({
   selector: 'app-my-boards',
   templateUrl: './my-boards.component.html',
-  styleUrls: ['./my-boards.component.css']
+  styleUrls: ['./my-boards.component.css'],
+  providers: [BoardService]
 })
 export class MyBoardsComponent implements OnInit {
+  public boards: object;
 
-  constructor() { }
+  constructor(private boardService: BoardService) {}
 
   ngOnInit() {
+    this.getMyBoards();
   }
 
+  getMyBoards(): void {
+    this.boardService.getBoards().subscribe(result => (this.boards = result));
+  }
 }
